843d3f0f2905f2c38f9ec25165f2b5f8e80f2887
Jeff Terrace <jterrace@gmail.com>
2011-12-07 14:01:28 -0500

    attempt at backporting subprocess.check_output to python 2.6

--- sirikata-cdn/celery_tasks/generate_metadata.py
+++ sirikata-cdn/celery_tasks/generate_metadata.py
@@ -13,6 +13,30 @@ import posixpath
 import tempfile
 import subprocess
 
+try:
+    subprocess.check_output
+except AttributeError:
+    def _check_output(*popenargs, **kwargs):
+        r"""Run command with arguments and return its output as a byte string.
+    
+        Backported from Python 2.7 as it's implemented as pure python on stdlib.
+    
+        >>> check_output(['/usr/bin/python', '--version'])
+        Python 2.6.2
+        """
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            error = subprocess.CalledProcessError(retcode, cmd)
+            error.output = output
+            raise error
+        return output
+    subprocess.check_output = _check_output
+
 @task
 def generate_metadata(filename, typeid):
     metadata = get_file_metadata(filename)
